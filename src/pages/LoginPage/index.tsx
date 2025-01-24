39c45ac2614cffe1451d7c24bdd555aa68db7054
Zikri Akmal S <zikriakmale@gmail.com>
2025-01-24 17:24:22 +0700

    feat: add fontawesome icon for some input

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,3 +1,5 @@
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import reactLogo from '../../assets/react.svg';
 
 const LoginPage = () => {
@@ -8,11 +10,17 @@ const LoginPage = () => {
                 <p className={`text-[#50b1a9] font-bold`}>Log-in to Your account</p>
             </div>
             <div className={`w-80 shadow rounded py-4 px-4 flex flex-col gap-4`}>
-                <div >
-                    <input type="text" className="w-full rounded border-[0.5px] px-2 py-1" />
+                <div className='relative'>
+                    <div className='absolute top-1 left-2'>
+                        <FontAwesomeIcon icon={faEnvelope} className='text-gray-500' />
+                    </div>
+                    <input type="text" className="pl-8 px-2 py-1 w-full rounded border-[0.5px]" />
                 </div>
-                <div >
-                    <input type="text" className="w-full rounded border-[0.5px] px-2 py-1" />
+                <div className='relative'>
+                    <div className='absolute top-1 left-2'>
+                        <FontAwesomeIcon icon={faLock} className='text-gray-500' />
+                    </div>
+                    <input type="text" className="pl-8 px-2 py-1 w-full rounded border-[0.5px]" />
                 </div>
                 <div>
                     <button onClick={() => console.log('login')} className={`cursor-pointer w-full h-8 bg-[#50b1a9] text-white text-xs rounded font-semibold hover:bg-cyan-900`}>
