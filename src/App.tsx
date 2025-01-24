b97b0da5fd7574cd4cada01ac0f68c0c9dfe5b9d
Zikri Akmal S <zikriakmale@gmail.com>
2025-01-24 17:44:38 +0700

    fix: router navigation to not login first

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import { Route, Routes } from 'react-router'
 import './App.css'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import { useEffect } from 'react'
+import TopicsPage from './pages/TopicsPage'
 
 function App() {
 
+  useEffect(()=>{
+
+  },[]);
+
   return (
     <Routes>
-      <Route index path='/login' element={<LoginPage />}></Route>
+      <Route path='/' index element={<TopicsPage />}></Route>
+      <Route path='/login' element={<LoginPage />}></Route>
       <Route path='/register' element={<RegisterPage />}></Route>
-      <Route path='/topics' index element={<RegisterPage />}></Route>
     </Routes>
   )
 }
