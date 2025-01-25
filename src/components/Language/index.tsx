22befdb72c24c5a9d983fadc8acb4965bb27a4f7
Zikri Akmal S <zikriakmale@gmail.com>
2025-01-25 09:33:39 +0700

    feat: language styling

--- src/components/Language/index.tsx
+++ src/components/Language/index.tsx
@@ -1,13 +1,13 @@
+import { faLanguage } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useContext } from "react";
 import Context from "../../context";
-import { faLanguage } from "@fortawesome/free-solid-svg-icons";
 
 const Language = () => {
     const context = useContext(Context);
     return (
-        <button onClick={() => context?.lang === 'en' ? context.setLang('it') : context?.setLang('en')} className='mb-5 cursor-pointer flex flex-row border-[0.5px] rounded px-2 items-center gap-3 shadow bg-white'>
-            <p> {context?.lang == 'en' ? 'it' : 'en'} </p>
+        <button onClick={() => context?.lang === 'en' ? context.setLang('it') : context?.setLang('en')} className='hover:bg-gray-400 hover:text-white hover:-translate-y-1.5 transition-all mb-5 cursor-pointer flex flex-row border-[0.5px] rounded px-2 items-center gap-3 shadow bg-white py-2'>
+            <p className="font-bold text-xs"> {context?.lang == 'en' ? 'Change language to (italia)' : 'change language to (english)'} </p>
             <FontAwesomeIcon icon={faLanguage} />
         </button>
     )
