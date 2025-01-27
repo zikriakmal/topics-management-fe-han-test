5f11b8cf7c53bb6cf8cafbc42fd699d6f8b1fbb5
Zikri Akmal S <zikriakmale@gmail.com>
2025-01-27 12:25:36 +0700

    feat: add topics form and form functionality

--- src/components/Topic/index.tsx
+++ src/components/Topic/index.tsx
@@ -1,11 +1,12 @@
 
 import { faThumbsDown, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import SampleImage from '../../assets/sample_post_image.png';
 import { useTranslation } from "react-i18next";
+import SampleImage from '../../assets/sample_post_image.png';
 
-const Topic = ({ title, createdBy, body, likePercentage, dislikePercentage, likeAction }:
+const Topic = ({ id, title, createdBy, body, likePercentage, dislikePercentage, likeAction }:
     {
+        id: number,
         title: string,
         createdBy: string,
         body: string,
@@ -22,10 +23,10 @@ const Topic = ({ title, createdBy, body, likePercentage, dislikePercentage, like
             </div>
             <div className="flex flex-col items-center flex-1">
                 <p className="font-semibold text-md">{title}</p>
-                <p className="font-light text-sm">{createdBy}</p>
+                <p className="font-light text-sm">Created By {createdBy}</p>
                 <p className="font-medium text-xs text-center mt-3">{body}</p>
                 <div className="flex flex-row mt-2 gap-4">
-                    <button onClick={() => likeAction({ id: 1, isLike: true })} className="hover:bg-red-900 hover:-translate-y-0.5 cursor-pointer flex-row text-xs text-white bg-red-600 flex gap-4 rounded border border-red-600 overflow-hidden">
+                    <button onClick={() => likeAction({ id: id, isLike: true })} className="hover:bg-red-900 hover:-translate-y-0.5 cursor-pointer flex-row text-xs text-white bg-red-600 flex gap-4 rounded border border-red-600 overflow-hidden">
                         <div className="flex flex-row p-2 py-1 items-center justify-center gap-2 flex-1">
                             <FontAwesomeIcon icon={faThumbsUp} />
                             <p className="font-bold">{t('topics.like_button')}</p>
@@ -34,7 +35,7 @@ const Topic = ({ title, createdBy, body, likePercentage, dislikePercentage, like
                             <p className="text-red-600 ">{likePercentage}%</p>
                         </div>
                     </button>
-                    <button onClick={() => likeAction({ id: 1, isLike: false })} className="hover:bg-blue-200 hover:-translate-y-0.5 transition-all cursor-pointer flex-row text-xs text-blue-400 flex gap-4 rounded border border-blue-400 overflow-hidden">
+                    <button onClick={() => likeAction({ id: id, isLike: false })} className="hover:bg-blue-200 hover:-translate-y-0.5 transition-all cursor-pointer flex-row text-xs text-blue-400 flex gap-4 rounded border border-blue-400 overflow-hidden">
                         <div className="flex flex-row p-2 py-1 items-center justify-center gap-2 flex-1">
                             <FontAwesomeIcon icon={faThumbsDown} />
                             <p className="font-bold">{t('topics.dislike_button')}</p>
--- src/pages/TopicsPage/index.tsx
+++ src/pages/TopicsPage/index.tsx
@@ -1,91 +1,104 @@
 import { faAdd, faSignOut } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useContext, useRef } from "react";
+import { Formik, FormikProps } from "formik";
+import { useContext, useEffect, useRef, useState } from "react";
+import { useTranslation } from "react-i18next";
 import Language from "../../components/Language";
 import Modal from "../../components/Modal";
 import Topic from "../../components/Topic";
 import Context from "../../context";
-import { useTranslation } from "react-i18next";
+import { createTopic, getAllTopic } from "../../services/topic";
 
-interface TopicList {
+interface TopicDetail {
+    id: number;
     title: string;
-    createdBy: string;
-    body: string;
-    likePercentage: string;
-    dislikePercentage: string;
+    description: string;
+    createdAt: string;
+    updatedAt: string;
+    user: {
+        id: number,
+        username: string,
+        name: string,
+    },
+    formattedUpdatedAtDifference: string,
 }
 
-const topicListData: Array<TopicList> = [
-    {
-        title: "Spring boot Spring-Security",
-        createdBy: "Created By Louay Yahyaoui",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-        likePercentage: "0",
-        dislikePercentage: "0"
-    },
-    {
-        title: "Spring boot Microservices",
-        createdBy: "Created By Louay Yahyaoui",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dymmy text ever since the 1500s",
-        likePercentage: "0",
-        dislikePercentage: "0"
-    },
-    {
-        title: "React Js",
-        createdBy: "Created By Louay Yahyaoui",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dymmy text ever since the 1500s",
-        likePercentage: "10",
-        dislikePercentage: "90"
-    },
-    {
-        title: "React Redux Toolkit",
-        createdBy: "Created By Louay Yahyaoui",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dymmy text ever since the 1500s",
-        likePercentage: "0",
-        dislikePercentage: "0"
-    },
-    {
-        title: "React Redux Toolkit",
-        createdBy: "Created By Louay Yahyaoui",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dymmy text ever since the 1500s",
-        likePercentage: "0",
-        dislikePercentage: "0"
-    }
-]
+interface topicFormValues {
+    title: string;
+    description: string;
+}
 
 const TopicsPage = () => {
     const context = useContext(Context);
+    const [errorsApi, setErrorsApi] = useState<string>("")
     const modalRef = useRef<{ open: () => void, close: () => void }>(null);
+    const formikRef = useRef<FormikProps<topicFormValues>>(null);
+
+    const [topics, setTopics] = useState<Array<TopicDetail>>([]);
     const { t } = useTranslation();
 
-    const submitForm = () => {
-        modalRef?.current?.close();
+    useEffect(() => {
+        getAll();
+    }, [])
+
+    const getAll = async () => {
+        getAllTopic().then((data) => {
+            setTopics(data?.data?.data)
+        })
     }
 
     const likeTopic = ({ id, isLike }: { id: number, isLike: boolean }) => {
         console.log(id, isLike, "tessst");
     }
 
+    const initialValues: topicFormValues = {
+        title: '',
+        description: '',
+    };
+
+    const submitForm = async (values: topicFormValues) => {
+        try {
+            await createTopic({ title: values.title, description: values.description });
+            modalRef?.current?.close();
+            getAll();
+            formikRef?.current?.resetForm();
+        } catch (e: any) {
+            setErrorsApi(e?.response?.data?.errors ?? "");
+        };
+    }
+
     return (
         <div>
-            <Modal title="Add new Topic" ref={modalRef}>
-                <form action="">
-                    <div className="px-3 py-2 flex flex-col gap-3">
-                        <div>
-                            <p className="text-sm mb-1"> {t('topics.form_title_label')}<span className="text-red-500">*</span></p>
-                            <input placeholder={t('topics.form_title_label')} type="text" className=" px-2 py-1 w-full rounded border-[0.5px] border-gray-300" />
-                        </div>
-                        <div>
-                            <p className="text-sm mb-1"> {t('topics.form_description_label')} <span className="text-red-500">*</span></p>
-                            <textarea placeholder={t('topics.form_description_label')} className=" px-2 py-1 w-full rounded border-[0.5px] border-gray-300" />
-                        </div>
-                        <div className="flex flex-row-reverse flex-1">
-                            <button type='button' onClick={() => submitForm()} className={`cursor-pointer px-6 h-8 bg-green-500 text-white text-xs rounded font-semibold hover:bg-cyan-900`}>
-                                {t('topics.form_save_button')}
-                            </button>
-                        </div>
-                    </div>
-                </form>
+            <Modal title={t('topics.add_new_button')} ref={modalRef}>
+                <Formik innerRef={formikRef}
+                    onSubmit={(e) => { submitForm(e); }}
+                    initialValues={initialValues} >
+                    {({ values, handleChange, handleSubmit }) => {
+                        return (
+                            <form onSubmit={handleSubmit}>
+                                <div className="px-3 py-2 flex flex-col gap-3">
+                                    <div>
+                                        <p className="text-sm mb-1"> {t('topics.form_title_label')}<span className="text-red-500">*</span></p>
+                                        <input onChange={handleChange} value={values.title} placeholder={t('topics.form_title_label')} name="title" type="text" className=" px-2 py-1 w-full rounded border-[0.5px] border-gray-300" />
+                                    </div>
+                                    <div>
+                                        <p className="text-sm mb-1"> {t('topics.form_description_label')} <span className="text-red-500">*</span></p>
+                                        <textarea onChange={handleChange} value={values.description} placeholder={t('topics.form_description_label')} name="description" className=" px-2 py-1 w-full rounded border-[0.5px] border-gray-300" />
+                                    </div>
+                                    <div>
+                                        {errorsApi !== "" ? <p className='text-sm text-red-500'>{errorsApi}</p> : null}
+                                    </div>
+                                    <div className="flex flex-row-reverse flex-1">
+                                        <button type='submit' className={`cursor-pointer px-6 h-8 bg-green-500 text-white text-xs rounded font-semibold hover:bg-cyan-900`}>
+                                            {t('topics.form_save_button')}
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        )
+                    }}
+
+                </Formik>
             </Modal>
             <div className="container mx-auto lg:max-w-3xl  flex-1 flex flex-col  justify-center">
                 <div className="mt-5 justify-items-center">
@@ -105,14 +118,15 @@ const TopicsPage = () => {
                     </div>
                 </div>
                 <div className="flex flex-col mx-4 my-10 gap-12">
-                    {topicListData.map((dt: TopicList, index: number) =>
+                    {topics.map((dt: TopicDetail, index: number) =>
                         <Topic
+                            id={dt.id}
                             key={index}
-                            likePercentage={dt.likePercentage}
-                            dislikePercentage={dt.dislikePercentage}
+                            likePercentage={"0"}
+                            dislikePercentage={"0"}
                             title={dt.title}
-                            createdBy={dt.createdBy}
-                            body={dt.body}
+                            createdBy={dt.user.name}
+                            body={dt.description}
                             likeAction={likeTopic}
                         />
                     )}
